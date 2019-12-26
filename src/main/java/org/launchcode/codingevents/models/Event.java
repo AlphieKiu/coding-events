4a7333443a9a18f2dcca38cde3d2623f88cbbfdd
AlphieKiu <kiuhuaien87@gmail.com>
2019-12-26 16:15:55 -0600

    Done with Ch13 videos

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,20 +1,47 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
     private int id;
     private static int nextId = 1;
+
+    @NotBlank (message="Name is required!")
+    @Size(min = 3, max = 50, message="Name must be between 3-50 characters.")
     private String name;
+
+    @Size(max=500, message="Description too long!")
     private String description;
 
-    public Event(String name, String description) {
+    @NotBlank(message="Email is required!")
+    @Email(message="Invalid email. Try again.")
+    private String contactEmail;
+
+    private EventType type;
+
+
+
+    public Event(String name, String description, String contactEmail, EventType type) {
         this.name = name;
         this.description = description;
+        this.contactEmail= contactEmail;
+        this.type = type;
         this.id = nextId;
         nextId++;
     }
 
+    public Event() {}
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
+    }
     public String getDescription() {
         return description;
     }
@@ -25,6 +52,14 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     public void setName(String name) {
